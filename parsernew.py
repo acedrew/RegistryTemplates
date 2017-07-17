73cd722ae65e008d55a2290a89ffc8f1754507b6
Andrew Rodgers <acedrew@gmail.com>
2017-07-17 01:56:10 +0000

    refactored to functions, made importable

--- parsernew.py
+++ parsernew.py
@@ -1,41 +1,47 @@
 import csv
 import argparse
 
-arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument(
-           "c_f_1", type=argparse.FileType('r'), help="I/P devices.csv")
-arg_parser.add_argument(
-           "c_f_2", type=argparse.FileType('r'), help="I/P pointlist.csv")
-args = arg_parser.parse_args()
 
-reader = csv.DictReader(args.c_f_1, delimiter=",")
-devices = []
-for row in reader:
-    devices.append(row)
-
-reader = csv.DictReader(args.c_f_2, delimiter=",")
-points = []
-for row in reader:
-    points.append(row)
-
-device_files = []
-for device in devices:
-    device_registry = []
-    for point in points:
-        device_registry.append(dict(point, **device))
-    device_files.append(device_registry)
-
-for registry in device_files:
-    with open(registry[0]['volttron device name'] + ".csv", "w+") as f:
-        writer = csv.DictWriter(
-                 f, delimiter=",", fieldnames=registry[0].keys())
-        writer.writeheader()
-        for row in registry:
-            writer.writerow(row)
+def read_registry(filename):
+    reader = csv.DictReader(filename, delimiter=",")
+    registry = []
+    for row in reader:
+        registry.append(row)
+    return registry
 
 
+def write_device_files(device_files):
+    for registry in device_files:
+        with open(registry[0]['volttron device name'] + ".csv", "w+") as f:
+            writer = csv.DictWriter(f, delimiter=",",
+                                    fieldnames=registry[0].keys())
+            writer.writeheader()
+            for row in registry:
+                writer.writerow(row)
 
 
+def create_device_files(devices, points):
+    device_files = []
+    for device in devices:
+        device_registry = []
+        for point in points:
+            device_registry.append(dict(point, **device))
+        device_files.append(device_registry)
+    return device_files
 
 
+def main(args):
+    devices = read_registry(args.c_f_1)
+    points = read_registry(args.c_f_2)
+    device_files = create_device_files(devices, points)
+    write_device_files(device_files)
 
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("c_f_1", type=argparse.FileType('r'),
+                            help="I/P devices.csv")
+    arg_parser.add_argument("c_f_2", type=argparse.FileType('r'),
+                            help="I/P pointlist.csv")
+    args = arg_parser.parse_args()
+    main(args)
